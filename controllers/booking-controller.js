743c54229d2bf9544138f5b4c84bccfb9b440183
Balaji The Boss <brainsmartlabs@gmail.com>
2022-11-28 16:40:18 +0530

    implemented wallet transction and docker file

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Booking = require('../models/Booking.js');
 const Car = require('../models/Car.js');
+const User = require('../models/User.js');
 
 
 module.exports.bookACar = async (req, res) => {
@@ -8,20 +9,27 @@ module.exports.bookACar = async (req, res) => {
 
     req.body.transactionID = Math.floor((Math.random() * 1000000000)).toString();
 
+
     if (req.id !== req.body.user) {
         return res.status(400).json({ message: 'Un-Autharized Access' });
     }
 
     let newBooking;
     try {
+        let totalAmount = req.body.totalAmount;
         newBooking = new Booking(req.body);
         let car = await Car.findById(req.body.car);
+        let user = await User.findById(req.body.user);
+        //Start Transaction
         const session = await mongoose.startSession();
         session.startTransaction({ session });
         newBooking = await newBooking.save({ session });
         car.bookedTimeSlots.push(req.body.bookedTimeSlots);
         car = await car.save({ session });
+        user.wallet = user.wallet - totalAmount;
+        user = await user.save({ session });
         await session.commitTransaction();
+        //End Transaction
     } catch (err) {
         console.log(err);
         return res.status(500).json({ message: `This transaction has failed due to: ${err}` });
